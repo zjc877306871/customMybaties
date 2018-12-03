813153072b6f4bc7086c42c493891bb023a3d869
zhangjc <1204880901@qq.com>
2018-12-03 17:27:10 +0800

    mybatis  练习

---
+++ src/main/java/customBase/config/Configuration.java
@@ -0,0 +1,18 @@
+package customBase.config;/**
+ * Created by user on 2018/12/3.
+ */
+
+/**
+ * @ClassName Configuration
+ * @Description TODO
+ * @Autor user
+ * @Date 2018/12/3 17:12
+ * @Version 1.0
+ **/
+public class Configuration {
+
+
+
+
+
+}
---
+++ src/main/java/practice/jdbc/PreJdbc.java
@@ -0,0 +1,30 @@
+package practice.jdbc;/**
+ * Created by user on 2018/12/3.
+ */
+
+/**
+ * @ClassName PreJdbc
+ * @Description TODO
+ * @Autor user
+ * @Date 2018/12/3 17:16
+ * @Version 1.0
+ **/
+public class PreJdbc {
+
+
+    public void preStatem(){
+
+        //加载数据驱动
+
+        //从DriverManager获取数据库连接  Connection
+
+        //获取PremSatement对象
+
+        //执行sql
+
+        //为占位符设值
+
+        //解析数据库返回结果  resultMap
+
+    }
+}
